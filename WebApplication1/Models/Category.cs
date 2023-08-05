7c868e9b801dddd720ee4864d31f78d0312e70e1
Dinamow <meemoo102039@gmail.com>
2023-08-05 09:12:27 +0300

    migration and data base

---
+++ WebApplication1/Models/Category.cs
@@ -0,0 +1,11 @@
+﻿using System.Reflection.Metadata.Ecma335;
+
+namespace WebApplication1.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public ICollection<PokemonCategory> pokemonCategories { get; set; }
+    }
+}
---
+++ WebApplication1/Models/Country.cs
@@ -0,0 +1,9 @@
+﻿namespace WebApplication1.Models
+{
+    public class Country
+    {
+        public string Name { get; set; }
+        public int Id { get; set; }
+        public ICollection<Owner> Owners { get; set; }
+    }
+}
