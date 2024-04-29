f0740086b021fd0f849c7a4c0db761514b6c61df
DINAMOW <meemoo102039@gmail.com>
2024-04-29 10:16:33 +0300

    cleanup, add gitignore

--- WebApplication1/obj/Debug/net7.0/WebApplication1.AssemblyInfo.cs
+++ WebApplication1/obj/Debug/net7.0/WebApplication1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6c524ec9ab1a9e0ad47dc64e420b365dfe86857e")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
