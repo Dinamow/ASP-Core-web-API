e613c4b9520b65dd038be52fe703eef97a6ce5e0
DINAMOW <meemoo102039@gmail.com>
2024-05-02 12:13:11 +0300

    init: the User module for the db tables

---
+++ loginService/Models/User.cs
@@ -0,0 +1,74 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace loginService.Models
+{
+    public class User
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public Guid Id { get; set; }
+        [Required(ErrorMessage = "Username is required")]
+        public required string Username { get; set; }
+        [Required(ErrorMessage = "Password is required")]
+        private string? Password { get; set; }
+        [Required(ErrorMessage = "Email is required")]
+        [EmailAddress(ErrorMessage = "Invalid email address")]
+        public required string Email { get; set; }
+        [Required(ErrorMessage = "Phone is required")]
+        public required string Phone { get; set; }
+        [Required(ErrorMessage = "Gender is required")]
+        public required string Gender { get; set; }
+        [Required(ErrorMessage = "Role is required")]
+        public required string Role { get; set; }
+        public bool IsActive { get; set; } = false;
+        public string? SessionId { get; set; }
+        public string? ResetToken { get; set; }
+        public string? ResetTokenExpire { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+        public ICollection<User>? Users { get; set; }
+        public ICollection<User>? RecivedConnection { get; set; }
+        public void setPassword(string password)
+        {
+            if (string.IsNullOrEmpty(password))
+            {
+                throw new Exception("Password is required");
+            }
+            if (password.Length < 8)
+            {
+                throw new Exception("Password must be at least 8 characters long");
+            }
+            if (!password.Any(char.IsDigit))
+            {
+                throw new Exception("Password must contain at least one digit");
+            }
+            if (!password.Any(char.IsUpper))
+            {
+                throw new Exception("Password must contain at least one uppercase letter");
+            }
+            if (!password.Any(char.IsLower))
+            {
+                throw new Exception("Password must contain at least one lowercase letter");
+            }
+            this.Password = HashPassword(password);
+        }
+        private string HashPassword(string password)
+        {
+            // Generate a salt
+            string salt = BCrypt.Net.BCrypt.GenerateSalt();
+
+            // Hash the password with the salt
+            string hashedPassword = BCrypt.Net.BCrypt.HashPassword(password, salt);
+
+            return hashedPassword;
+        }
+        public bool VerifyPassword(string password)
+        {
+            // Verify the password against the hashed password
+            bool passwordMatches = BCrypt.Net.BCrypt.Verify(password, this.Password);
+
+            return passwordMatches;
+        }
+    }
+}
---
+++ loginService/Models/UserConnection.cs
@@ -0,0 +1,11 @@
+﻿namespace loginService.Models
+{
+    public class UserConnection
+    {
+        public Guid User1Id { get; set; }
+        public required User User1 { get; set; }
+
+        public Guid User2Id { get; set; }
+        public required User User2 { get; set; }
+    }
+}
