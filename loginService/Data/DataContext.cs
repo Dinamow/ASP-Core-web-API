ee78f4b46e09589b013739eb0ca114dbdee95526
DINAMOW <meemoo102039@gmail.com>
2024-05-02 12:13:51 +0300

    init: migrations

---
+++ loginService/Data/DataContext.cs
@@ -0,0 +1,59 @@
+﻿using loginService.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace loginService.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+        public DbSet<User> Users { get; set; }
+        public DbSet<UserConnection> UserConnections { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>(entity =>
+            {
+                entity.Property(u => u.Id).ValueGeneratedOnAdd();
+                entity.HasKey(u => u.Id);
+                entity.HasAlternateKey(u => u.Email);
+                entity.HasAlternateKey(u => u.Username);
+            });
+            modelBuilder.Entity<UserConnection>(entity =>
+            {
+                entity.HasKey(uc => new { uc.User1Id, uc.User2Id });
+
+                entity.HasOne(uc => uc.User1)
+                      .WithMany()
+                      .HasForeignKey(uc => uc.User1Id)
+                      .IsRequired()
+                      .OnDelete(DeleteBehavior.Restrict);
+
+                entity.HasOne(uc => uc.User2)
+                      .WithMany()
+                      .HasForeignKey(uc => uc.User2Id)
+                      .IsRequired()
+                      .OnDelete(DeleteBehavior.Restrict);
+            });
+        }
+        private static User CreateDefaultUser()
+        {
+            var user = new User()
+            {
+                Username = "DINAMOW",
+                Email = "meemoo102039@gmail.com",
+                Phone = "+201208677550",
+                Role = "Admin",
+                Gender = "male"
+            };
+            user.setPassword("Aadmin123");
+            return user;
+        }
+
+        // Seed the database
+        public void SeedData()
+        {
+            var user = CreateDefaultUser();
+            Users.Add(user);
+            SaveChanges();
+        }
+    }
+}
---
+++ loginService/Migrations/20240502082254_Init.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace loginService.Migrations
+{
+    /// <inheritdoc />
+    public partial class Init : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Username = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    Phone = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Gender = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Role = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    IsActive = table.Column<bool>(type: "bit", nullable: false),
+                    SessionId = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ResetToken = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ResetTokenExpire = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    UpdatedAt = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                    table.UniqueConstraint("AK_Users_Email", x => x.Email);
+                    table.UniqueConstraint("AK_Users_Username", x => x.Username);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "UserConnections",
+                columns: table => new
+                {
+                    User1Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    User2Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserConnections", x => new { x.User1Id, x.User2Id });
+                    table.ForeignKey(
+                        name: "FK_UserConnections_Users_User1Id",
+                        column: x => x.User1Id,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_UserConnections_Users_User2Id",
+                        column: x => x.User2Id,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "UserUser",
+                columns: table => new
+                {
+                    RecivedConnectionId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    UsersId = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserUser", x => new { x.RecivedConnectionId, x.UsersId });
+                    table.ForeignKey(
+                        name: "FK_UserUser_Users_RecivedConnectionId",
+                        column: x => x.RecivedConnectionId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_UserUser_Users_UsersId",
+                        column: x => x.UsersId,
+                        principalTable: "Users",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserConnections_User2Id",
+                table: "UserConnections",
+                column: "User2Id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserUser_UsersId",
+                table: "UserUser",
+                column: "UsersId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "UserConnections");
+
+            migrationBuilder.DropTable(
+                name: "UserUser");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
