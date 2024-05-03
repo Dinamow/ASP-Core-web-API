40d2e9edfa4bde191ae0bb9ec6a4e2c8438ec8a9
DINAMOW <meemoo102039@gmail.com>
2024-05-03 20:46:31 +0300

    Create: the Needed Methods for now

---
+++ loginService/Interfaces/IUserRepository.cs
@@ -0,0 +1,19 @@
+﻿using loginService.Models;
+
+namespace loginService.Interfaces
+{
+    public interface IUserRepository
+    {
+        bool UserExists(User user);
+        Task<User> CreateUser(User user);
+        Task<User> UpdateUser(User user, Dictionary<string, string>? filter = null);
+        bool DeleteUser(User user);
+        Task<int> Save();
+        List<User> GetUsers();
+        Task<User> GetUserById(Guid id);
+        Task<User> GetUserBy(Dictionary<string, string> filter);
+        Task<string> Login(string email, string password);
+        Task<bool> Logout(string sessionId);
+        Task SignUp(Dictionary<string, string> dic);
+    }
+}
