dbd7b7eea0a797bb2d761042ffdf7f77f3ae2085
DINAMOW <meemoo102039@gmail.com>
2024-05-03 20:49:17 +0300

    Implement: the mapping

---
+++ loginService/Helper/MappingProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using loginService.Dto;
+using loginService.Models;
+
+namespace loginService.Helper
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<User, UserDto>();
+            CreateMap<UserDto, User>();
+        }
+    }
+}
