e4a67ec79f08d62b5129486e40c91d3ca001acf3
DINAMOW <meemoo102039@gmail.com>
2024-05-03 20:48:45 +0300

    Create: mapping profile

---
+++ loginService/Dto/UserDto.cs
@@ -0,0 +1,17 @@
+﻿using loginService.Models;
+
+namespace loginService.Dto
+{
+    public class UserDto
+    {
+        public Guid Id { get; set; }
+        public required string Username { get; set; }
+        public required string Email { get; set; }
+        public required string Phone { get; set; }
+        public required string Gender { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
+        public ICollection<Connection>? SentConnections { get; set; }
+        public ICollection<Connection>? ReceivedConnections { get; set; }
+    }
+}
