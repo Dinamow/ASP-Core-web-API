e5d4ff71c38f348806587fc2a9e6d1a74c4703cb
DINAMOW <meemoo102039@gmail.com>
2024-05-03 20:51:53 +0300

    Improvment: Add Scope, AutoMapper

--- loginService/Program.cs
+++ loginService/Program.cs
@@ -1,5 +1,8 @@
-using loginService.Data;
+﻿using loginService.Data;
 using Microsoft.EntityFrameworkCore;
+using loginService.Repository;
+using loginService.Interfaces;
+using loginService.Helper;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,10 +12,14 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddAutoMapper(typeof(MappingProfile).Assembly);
+builder.Services.AddScoped<IUserRepository, UserRepository>();
 builder.Services.AddDbContext<DataContext>(options =>
 {
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+
+// Configure session to use in-memory storage
 
 var app = builder.Build();
 
